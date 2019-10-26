da713e2b3b81af9807cd949f839bccb2cfb2dbd6
Pascal.Offenhaeusser <pascalo@k15t.com>
2019-10-26 14:21:40 +0200

    annotations

--- src/main/java/de/hackerstolz/climathon/escapeapocalypse/api/model/U_Group.java
+++ src/main/java/de/hackerstolz/climathon/escapeapocalypse/api/model/U_Group.java
@@ -2,14 +2,19 @@ package de.hackerstolz.climathon.escapeapocalypse.api.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class U_Group {
     @Id
-    @GeneratedValue
+   @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @NotEmpty
     private String name;
 
     public U_Group() {
