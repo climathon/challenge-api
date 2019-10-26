e3679fc442f2fe626c2df3ccb727f382056210c3
chris <mail@iamchrisb.de>
2019-10-26 16:57:23 +0200

    search by userid - results

--- src/main/java/de/hackerstolz/climathon/escapeapocalypse/api/repository/ResultRepository.java
+++ src/main/java/de/hackerstolz/climathon/escapeapocalypse/api/repository/ResultRepository.java
@@ -1,7 +1,10 @@
 package de.hackerstolz.climathon.escapeapocalypse.api.repository;
 
+import java.util.List;
+
 import de.hackerstolz.climathon.escapeapocalypse.api.model.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResultRepository extends JpaRepository<Result, Long> {
+	List<Result> findByUserId(Long userId);
 }
